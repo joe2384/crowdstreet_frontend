49299125313f3f44056fac711a939eb4e7c0d213
Joseph Grounds <primemtn1@gmail.com>
2021-01-31 12:30:39 -0700

    Installed Redux, and created redux store.

---
+++ src/store/reducers/dataReducer.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../types/actionTypes';
+
+const initialState = {
+  data: {},
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.DATA_REQUEST_STARTED:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case actionTypes.DATA_REQUEST_FULFILLED:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case actionTypes.DATA_REQUEST_ERROR:
+      return {
+        ...state,
+        data: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
