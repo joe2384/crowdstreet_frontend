55d8ac7f301ed45e963a89a9a9aae689d6be7aa3
Joseph Grounds <primemtn1@gmail.com>
2021-01-31 14:24:03 -0700

    removed or renamed files.

--- src/containers/investment_funnel/funnel.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment, useState } from 'react';
-import HocApiComponent from '../hocApiComponent';
-
-const Funnel = ({ data, funnelAction }) => {
-  console.log('Log = ', funnelAction);
-  return <Fragment>Test</Fragment>;
-};
-
-export default HocApiComponent(Funnel);
